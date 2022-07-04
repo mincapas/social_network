33c39ec2e326b2f82ca577baab8af344baa4956d
Mindaugas <mincapas@yahoo.com>
2022-07-04 20:48:44 +0300

    Added wall model, didn't update to URLS yet. Trying out push

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Wall(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique wall object ID')
+    book = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    name = models.TextField('Name', max_length=255, help_text='Name of the wall')
+    pin_to_top = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
